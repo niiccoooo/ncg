10686fde4c051dc7038d0bda7525cea1a226d014
Gregor Pfeifer <gregor.pfeifer.gp@gmail.com>
2019-11-19 19:26:56 +0000

    p2-2 with do ... while

--- p2_2.c
+++ p2_2.c
@@ -2,20 +2,20 @@
 
 int main () {
 
+	int i;
+	int eingegebeneZahl;
+    int falscheEingabe;
+
 	printf("Programm zur Primzahlausgabe nach dem Prinzip: ""Sieb des Eratosthenes""\n");
 
-	int i;;
-	int eingegebeneZahl;
-	
-	
-	for (i = 0; i < 1; i++){
+	do {
 		printf("Bitte geben sie eine Zahl von 2 - 1000 ein:  "); 
 		scanf("%i", &eingegebeneZahl);
-			if ( eingegebeneZahl < 2 || eingegebeneZahl > 1000){
+        falscheEingabe = (eingegebeneZahl < 2 || eingegebeneZahl > 1000); 
+		if (falscheEingabe) {
 			printf("Fehler! Bitte Eingabebedingungen beachten!\n");
-			i--;
-			}
-	}
+		}
+	} while (falscheEingabe);
 	return 0;
 }	
 
