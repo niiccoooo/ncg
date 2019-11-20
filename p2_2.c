29fe1494ca2d762f0bb30a0cfe4293563a263c98
niiccoooo <57960553+niiccoooo@users.noreply.github.com>
2019-11-20 21:31:15 +0100

    Update p2_2.c

--- p2_2.c
+++ p2_2.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main () {
 
 	int i;
 	int j;
 	int eingegebeneZahl;
-   	 int falscheEingabe;
-    	const int arraysize = 999;
-   	 int markiert[arraysize];
-   	 int counter = 0;
+    int falscheEingabe;
+    bool markiert[1000] = {0};
 
 	printf("Programm zur Primzahlausgabe nach dem Prinzip: ""Sieb des Eratosthenes""\n");
 
@@ -21,20 +20,31 @@ int main () {
 		}
 	} while (falscheEingabe);
 
-	for(i = 0; i <= arraysize; i++) {
-        if (markiert[i-2]) {
-               for (j = 2 * i; j <= arraysize; j += i) {
-                    markiert[j-2] == 0;
+	for(i = 2; i <= eingegebeneZahl; i++) {
+        if (!markiert[i-2]) {
+               for (j = 2 * i; j <= eingegebeneZahl; j += i) {
+                    markiert[j-2] = true;
                }
         }
 	}
-	for (i = 0; i <= arraysize; i++) {
-        if (markiert[i] = 1) {
+    int primzahlen[1000];
+	int counter = 0;
+
+	for (i = 2; i <= eingegebeneZahl; i++) {
+        if (!markiert[i-2]) {
+        primzahlen[counter] = i;
         counter++;
         }
 	}
+
+    printf("\n");
 	printf("In der Zahl %i gibt es %i Primzahlen\n", eingegebeneZahl, counter);
 	printf("Die Primzahlen der Zahl %i sind:\n", eingegebeneZahl);
+	for (i = 0; i <= eingegebeneZahl; i++) {
+        if (markiert[i] = 1) {
+            printf("%i, ", primzahlen[i]);
+        }
+	}
 	printf("\n");
 	return 0;
 }
