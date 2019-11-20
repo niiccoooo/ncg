fae4b7536e8c9e0683e850d41e966fe55ac70689
niiccoooo <57960553+niiccoooo@users.noreply.github.com>
2019-11-20 13:39:54 +0100

    Update p2_4.c

--- p2_4.c
+++ p2_4.c
@@ -11,13 +11,14 @@ void swap(int *x, int *y) {
 void bubbleSort (int array[], int n) {
 
 	int i, j;
-	for (i = 0; i < n-1; i++);
+	for (i = 0; i < n-1; i++) {
 
-	for (j = 0; i < n-i-1; j++) {
-		if (array[j] > array[j+1]) {
-		swap (&array[j], &array[j+1]);
+        for (j = 0; j < n-1; j++) {
+            if (array[j] > array[j+1]) {
+            swap (&array[j], &array[j+1]);
+            }
 		}
-		}
+	}
 }
 
 void printArray (int array[], int size) {
@@ -35,17 +36,17 @@ int main (){
 	int i;
 	const int zahlengroesse = 6;
 	int zahlenfolge[zahlengroesse];
-	int n = sizeof(zahlenfolge)/sizeof(zahlenfolge[0]);
+    int n = sizeof(zahlenfolge)/sizeof(zahlenfolge[0]);
 
 	for (i = 0; i < zahlengroesse; i++) {
-		printf("Bitte geben Sie Zahl %i ein\n", i+1);	
+		printf("Bitte geben Sie Zahl %i ein\n", i+1);
 		scanf("%i", &zahlenfolge[i]);
-	}	
+	}
+
 	printf("Ungeordnetes Arrray:\n");
 	printArray(zahlenfolge, n);
 	printf("\n\nGeordnetes Array:\n");
 	bubbleSort(zahlenfolge, n);
-	printArray(zahlenfolge, n);	
+	printArray(zahlenfolge, n);
 	return 0;
-} 
-
+}
