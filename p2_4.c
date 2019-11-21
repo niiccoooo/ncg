f7c926f461c19fa9dd7cf98fc4a8f7311fca1614
niiccoooo <57960553+niiccoooo@users.noreply.github.com>
2019-11-21 21:30:14 +0100

    Update p2_4.c

--- p2_4.c
+++ p2_4.c
@@ -13,10 +13,10 @@ void bubbleSort (int array[], int n) {
 	int i, j;
 	for (i = 0; i < n-1; i++) {
 
-        for (j = 0; j < n-1; j++) {
-            if (array[j] > array[j+1]) {
-            swap (&array[j], &array[j+1]);
-            }
+		for (j = 0; j < n-1; j++) {
+         		if (array[j] > array[j+1]) {
+            			swap (&array[j], &array[j+1]);
+            		}
 		}
 	}
 }
